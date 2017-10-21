9617553acb8146163b23da117cb36fd7199c99e4
song <lys211@yeah.net>
2017-10-21 21:36:31 +0800

    添加WIFI直连相关的代码
    添加WIFI直连相关的代码
    本意是两个设备之间互相连接，然后通过ProtoBuf，Socket来传输数据的。
    暂时只实现了查找可wifi直连的设备的功能

--- app/src/main/java/zms/song/illustrates/widget/toast/HelloToast.java
+++ app/src/main/java/zms/song/illustrates/widget/toast/HelloToast.java
@@ -1,12 +1,19 @@
 package zms.song.illustrates.widget.toast;
 
 import android.content.Context;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import zms.song.illustrates.R;
+import zms.song.illustrates.base.IllustratesApplication;
 
 /**
  * Created by song on 10/11/2017.
@@ -16,7 +23,11 @@ public class HelloToast {
 
     private Toast mToast;
 
-    private HelloToast(Context context, CharSequence text, int duration) {
+    @IntDef({Toast.LENGTH_SHORT, Toast.LENGTH_LONG})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ToastDuration {}
+
+    private HelloToast(Context context, CharSequence text, @ToastDuration int duration) {
         View v = LayoutInflater.from(context).inflate(R.layout.hello_toast, null);
         TextView textView = (TextView) v.findViewById(R.id.center_text_view);
         textView.setText(text);
@@ -25,8 +36,38 @@ public class HelloToast {
         mToast.setView(v);
     }
 
-    public static HelloToast makeText(Context context, CharSequence text, int duration) {
+    public static HelloToast makeText(Context context, CharSequence text, @ToastDuration int duration) {
         return new HelloToast(context, text, duration);
+    }
+
+    public static HelloToast makeText(CharSequence text, @ToastDuration int duration) {
+        return new HelloToast(IllustratesApplication.getIllustratesApplication(), text, duration);
+    }
+
+    public static HelloToast makeText(@NonNull Context context, CharSequence text) {
+        return new HelloToast(context, text, Toast.LENGTH_SHORT);
+    }
+
+    public static HelloToast makeText(CharSequence text) {
+        return new HelloToast(IllustratesApplication.getIllustratesApplication(), text, Toast.LENGTH_SHORT);
+    }
+
+    public static HelloToast makeText(@NonNull Context context, @StringRes int message, @ToastDuration int duration) {
+        return new HelloToast(context, context.getString(message), duration);
+    }
+
+    public static HelloToast makeText(@StringRes int message, @ToastDuration int duration) {
+        return new HelloToast(IllustratesApplication.getIllustratesApplication(),
+                IllustratesApplication.getIllustratesApplication().getString(message), duration);
+    }
+
+    public static HelloToast makeText(@NonNull Context context, @StringRes int message) {
+        return new HelloToast(context, context.getString(message), Toast.LENGTH_SHORT);
+    }
+
+    public static HelloToast makeText(@StringRes int message) {
+        return new HelloToast(IllustratesApplication.getIllustratesApplication(),
+                IllustratesApplication.getIllustratesApplication().getString(message), Toast.LENGTH_SHORT);
     }
 
     public void show() {
